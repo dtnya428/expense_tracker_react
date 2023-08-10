c829909666cd3045e983cfd604b37a380f753303
Denise Nyamhingura <denise.nyamhingura@entelect.co.za>
2023-08-10 17:54:11 +0200

    Expense Item Component

---
+++ expense-tracker-react/src/components/expenseItem.js
@@ -0,0 +1,15 @@
+import './expenseItem.css'
+
+function expenseItem(props) {
+    return (
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>$ {props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default expenseItem;
