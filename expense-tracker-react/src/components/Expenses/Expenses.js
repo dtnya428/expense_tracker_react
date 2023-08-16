72c8205d497671974ea7d188806610e40487c36b
Denise Nyamhingura <denise.nyamhingura@entelect.co.za>
2023-08-16 06:51:56 +0200

    Separate componenets to componet files

--- expense-tracker-react/src/components/Expenses/Expenses.js
+++ expense-tracker-react/src/components/Expenses/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./expenseItem";
+import Card from "../UI/Card";
 
 function Expenses(expenses) {
 
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem expenses={expenses.expenses[0]}></ExpenseItem>
             <ExpenseItem expenses={expenses.expenses[1]}></ExpenseItem>
             <ExpenseItem expenses={expenses.expenses[2]}></ExpenseItem>
-        </div>
+        </Card>
     );
 }
 
--- expense-tracker-react/src/components/Expenses/expenseItem.js
+++ expense-tracker-react/src/components/Expenses/expenseItem.js
@@ -1,16 +1,17 @@
 import './expenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from "../UI/Card";
 
 function expenseItem(expenses) {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             {/* Date Component */}
             <ExpenseDate date={expenses.expenses.date} />
             <div className='expense-item__description'>
                 <h2>{expenses.expenses.title}</h2>
                 <div className='expense-item__price'>$ {expenses.expenses.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
